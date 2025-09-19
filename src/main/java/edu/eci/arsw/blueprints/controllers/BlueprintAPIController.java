5abb2492f50584dd16483b05ae4310f8fa69b4cc
EmilyNorena <emily.norena-c@mail.escuelaing.edu.co>
2025-09-19 05:50:56 -0500

    final

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -5,22 +5,101 @@
  */
 package edu.eci.arsw.blueprints.controllers;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author hcadavid
  */
-public class BlueprintAPIController {
-    
-    
-    
-    
-    
-}
 
+@RestController
+@RequestMapping(value = "/blueprints")
+public class BlueprintAPIController {
+
+    @Autowired
+    private BlueprintsServices blueprintServices;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getAllBlueprints() {
+        try {
+            Set<Blueprint> data = blueprintServices.getAllBlueprints();
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/{author}", method = RequestMethod.GET)
+    public ResponseEntity<?> getBlueprintsByAuthor(@PathVariable("author") String author) {
+        try {
+            Set<Blueprint> data = blueprintServices.getBlueprintsByAuthor(author);
+            if (data == null || data.isEmpty()) {
+                return new ResponseEntity<>("No se encontraron planos para dicho autor" + author, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } catch (BlueprintNotFoundException ex) {
+            return new ResponseEntity<>("Autor no encontrado", HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/{author}/{bpname}", method = RequestMethod.GET)
+    public ResponseEntity<?> getBlueprint(@PathVariable("author") String author,
+            @PathVariable("bpname") String bpname) {
+        try {
+            Blueprint data = blueprintServices.getBlueprint(author, bpname);
+            if (data == null) {
+                return new ResponseEntity<>("No se encontraron planos para dicho autor" + author, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } catch (BlueprintNotFoundException ex) {
+            return new ResponseEntity<>("Autor no encontrado", HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> createBlueprint(@RequestBody Blueprint bp) {
+        try {
+            blueprintServices.addNewBlueprint(bp);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("No se pudo registrar el plano", HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @RequestMapping(value = "/{author}/{bpname}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateBlueprint(@PathVariable("author") String author,
+                                             @PathVariable("bpname") String bpname,
+                                             @RequestBody Blueprint bp) {
+        try {
+            if (!author.equals(bp.getAuthor()) || !bpname.equals(bp.getName())) {
+                return new ResponseEntity<>("Los datos del plano no coinciden con la URL.", HttpStatus.BAD_REQUEST);
+            }
+
+            blueprintServices.updateBlueprint(author, bpname, bp);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
